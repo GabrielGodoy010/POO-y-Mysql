ec60d01e6aaacb7c8bb5c49ce0f77d7bd1ec279a
GabrielGodoy010 <89681414+GabrielGodoy010@users.noreply.github.com>
2022-04-19 23:29:45 -0600

    Add files via upload

---
+++ ConexionDB2.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <mysql.h>
+#include <iostream>
+
+	using namespace std;
+
+	class ConexionBD2 {
+	private: MYSQL* conectar;
+	public:
+		void abrir_conexion() {
+			conectar = mysql_init(0);
+			conectar = mysql_real_connect(conectar, "localhost", "root", "usr_empresa", "Empresa@123", 3306, NULL, 0);
+		}
+		MYSQL* getConectar() {
+			return conectar;
+		}
+		void cerrar_conexion() {
+			mysql_close(conectar);
+		}
+
+
+	};
+
+
